07137ceec78c493b5c72af839f77d247abacf679
YU-SIANG,CHEN <qbb225827455@gmail.com>
2022-09-13 00:35:21 +0800

    區別請求類別與資料庫文件

---
+++ src/main/java/com/example/demo/Model/ProductRequest.java
@@ -0,0 +1,21 @@
+package com.example.demo.Model;
+
+import lombok.Data;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Data
+// 參考
+// 驗證資料
+// https://ithelp.ithome.com.tw/articles/10275699
+public class ProductRequest {
+
+    @NotEmpty(message = "Product name is undefined.")
+    private String name;
+
+    @NotNull
+    @Min(value = 0, message = "Price should be greater or equal to 0.")
+    private Integer price;
+}
