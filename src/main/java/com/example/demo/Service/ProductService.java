9327bb4ba7a76e7548df2db0ff31154c858cb88d
YU-SIANG,CHEN <qbb225827455@gmail.com>
2022-09-12 21:39:31 +0800

    取代原本代替資料庫的 List ，換成 MongoDB

--- src/main/java/com/example/demo/Service/ProductService.java
+++ src/main/java/com/example/demo/Service/ProductService.java
@@ -5,48 +5,72 @@ import com.example.demo.Exception.UnprocessableEntity;
 import com.example.demo.Model.Product;
 import com.example.demo.Model.ProductQueryParameter;
 import com.example.demo.Repository.MockProductDAO;
+import com.example.demo.Repository.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class ProductService {
 
     @Autowired
-    private MockProductDAO productDAO;
+    private ProductRepo productRepo;
 
     public Product createProduct(Product request) {
 
-        boolean isIdExisted = productDAO.find(request.getId()).isPresent();
-        if (isIdExisted) {
-            throw new UnprocessableEntity("The id of the product is duplicated.");
-        }
-
         Product product = new Product();
-        product.setId(request.getId());
         product.setName(request.getName());
         product.setPrice(request.getPrice());
 
-        return productDAO.insert(product);
+        return productRepo.insert(product);
     }
 
     public Product getProduct(String id) {
-        return productDAO.find(id)
+        return productRepo.findById(id)
                 .orElseThrow(() -> new NotFound("Can't find product."));
     }
 
     public Product replaceProduct(String id, Product request) {
-        Product product = getProduct(id);
-        return productDAO.replace(product.getId(), request);
+
+        Product oProduct = getProduct(id);
+
+        Product product = new Product();
+        product.setId(oProduct.getId());
+        product.setName(request.getName());
+        product.setPrice(request.getPrice());
+
+        return productRepo.save(product);
     }
 
     public void deleteProduct(String id) {
-        Product product = getProduct(id);
-        productDAO.delete(product.getId());
+
+        productRepo.deleteById(id);
     }
 
-    public List<Product> getProducts(ProductQueryParameter param) {
-        return productDAO.find(param);
+    public List<Product> getProducts(ProductQueryParameter parameter) {
+
+        String keyword = Optional.ofNullable(parameter.getKeyword()).orElse("");
+        int priceFrom = Optional.ofNullable(parameter.getPriceFrom()).orElse(0);
+        int priceTo = Optional.ofNullable(parameter.getPriceTo()).orElse(Integer.MAX_VALUE);
+
+        Sort sort = genSort(parameter.getOrderBy(), parameter.getSortRule());
+
+        return productRepo.findByPriceBetweenAndNameLikeIgnoreCase(priceFrom, priceTo, keyword, sort);
+    }
+
+    private Sort genSort(String orderBy, String sortRule) {
+
+        Sort sort = Sort.unsorted();
+        if (Objects.nonNull(orderBy) || Objects.nonNull(sortRule)) {
+            Sort.Direction direction = Sort.Direction.fromString(sortRule);
+            sort = Sort.by(direction, orderBy);
+        }
+
+        return sort;
     }
 }
