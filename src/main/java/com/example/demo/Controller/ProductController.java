c8c8d740e54d570034a0478ae0358d9e09cdb751
YU-SIANG,CHEN <qbb225827455@gmail.com>
2022-09-12 21:10:47 +0800

    表示層：也就是 Controller，應該負責接收前端的請求（request），並請 Service 處理，最後將資料做回應（response）。回應的範疇包括狀態碼（status code）、標頭（header）與主體（body）等。

--- src/main/java/com/example/demo/Controller/ProductController.java
+++ src/main/java/com/example/demo/Controller/ProductController.java
@@ -2,6 +2,8 @@ package com.example.demo.Controller;
 
 import com.example.demo.Model.Product;
 import com.example.demo.Model.ProductQueryParameter;
+import com.example.demo.Service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -17,38 +19,28 @@ import java.util.stream.Collectors;
 @RequestMapping(value = "/products", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ProductController {
 
-    private final List<Product> productList = new ArrayList<>();
+    @Autowired
+    private ProductService productService;
 
     @GetMapping("/{id}")
     public ResponseEntity<Product> getProduct(@PathVariable("id") String id) {
 
-        Optional<Product> productOptional = productList.stream()
-                .filter(p -> p.getId().equals(id))
-                .findFirst();
+        Product product = productService.getProduct(id);
+        return ResponseEntity.ok().body(product);
+    }
 
-        if (productOptional.isPresent()) {
-            Product product = productOptional.get();
-            return ResponseEntity.ok().body(product);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+    @GetMapping
+    public ResponseEntity<List<Product>> getProducts(
+            @ModelAttribute ProductQueryParameter parameter) {
+
+        List<Product> products = productService.getProducts(parameter);
+        return ResponseEntity.ok().body(products);
     }
 
     @PostMapping
     public ResponseEntity<Product> createProduct(@RequestBody Product request) {
 
-        boolean isIdExisted = productList.stream()
-                .anyMatch(p -> p.getId().equals(request.getId()));
-        if (isIdExisted) {
-            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
-        }
-
-        Product product = new Product();
-        product.setId(request.getId());
-        product.setName(request.getName());
-        product.setPrice(request.getPrice());
-
-        productList.add(product);
+        Product product = productService.createProduct(request);
 
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
@@ -63,45 +55,15 @@ public class ProductController {
     public ResponseEntity<Product> replaceProduct(
             @PathVariable("id") String id, @RequestBody Product request) {
 
-        Optional<Product> productOptional = productList.stream()
-                .filter(p -> p.getId().equals(id))
-                .findFirst();
-
-        if (productOptional.isPresent()) {
-            Product product = productOptional.get();
-            product.setName(request.getName());
-            product.setPrice(request.getPrice());
-
-            return ResponseEntity.ok().body(product);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        Product product = productService.replaceProduct(id, request);
+        return  ResponseEntity.ok().body(product);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Product> deleteProduct(@PathVariable("id") String id) {
 
-        boolean isRemoved = productList.removeIf(p -> p.getId().equals(id));
-
-        return isRemoved
-                ? ResponseEntity.noContent().build()
-                : ResponseEntity.notFound().build();
-    }
-
-    @GetMapping
-    public ResponseEntity<List<Product>> getProducts(@ModelAttribute ProductQueryParameter parameter) {
-
-        String keyword = parameter.getKeyword();
-        String orderBy = parameter.getOrderBy();
-        String sortRule = parameter.getSortRule();
-        Comparator<Product> comparator = genSortComparator(orderBy, sortRule);
-
-        List<Product> products = productList.stream()
-                .filter(p -> p.getName().toUpperCase().contains(keyword.toUpperCase()))
-                .sorted(comparator)
-                .collect(Collectors.toList());
-
-        return ResponseEntity.ok().body(products);
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
     }
 
     private Comparator<Product> genSortComparator(String orderBy, String sortRule) {
@@ -120,27 +82,5 @@ public class ProductController {
         return sortRule.equalsIgnoreCase("desc")
                 ? comparator.reversed()
                 : comparator;
-    }
-
-    @PostConstruct
-    private void initDB() {
-        Product product1 = new Product();
-        product1.setId("A01");
-        product1.setName("AAA");
-        product1.setPrice(100);
-
-        Product product2 = new Product();
-        product2.setId("A02");
-        product2.setName("BBB");
-        product2.setPrice(100);
-
-        Product product3 = new Product();
-        product3.setId("A03");
-        product3.setName("CCC");
-        product3.setPrice(100);
-
-        productList.add(product1);
-        productList.add(product2);
-        productList.add(product3);
     }
 }
