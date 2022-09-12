ca84dbf9c1558f8dfa3445b7455d41a2a7434a95
YU-SIANG,CHEN <qbb225827455@gmail.com>
2022-09-12 21:38:21 +0800

    查詢方式 findByPriceBetweenAndNameLikeIgnoreCase

---
+++ src/main/java/com/example/demo/Repository/ProductRepo.java
@@ -0,0 +1,14 @@
+package com.example.demo.Repository;
+
+import com.example.demo.Model.Product;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepo extends MongoRepository<Product, String> {
+
+    List<Product> findByPriceBetweenAndNameLikeIgnoreCase(int priceFrom, int priceTo, String name, Sort sort);
+}
