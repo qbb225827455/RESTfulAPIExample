f11d0c47de4f5aad6d2423408c417993100feb3b
YU-SIANG,CHEN <qbb225827455@gmail.com>
2022-09-13 15:34:31 +0800

    轉換資料庫物件為回應物件

--- src/main/java/com/example/demo/Converter/ProductConverter.java
+++ src/main/java/com/example/demo/Converter/ProductConverter.java
@@ -2,6 +2,7 @@ package com.example.demo.Converter;
 
 import com.example.demo.Model.Product;
 import com.example.demo.Model.ProductRequest;
+import com.example.demo.Model.ProductResponse;
 
 public class ProductConverter {
 
@@ -14,5 +15,14 @@ public class ProductConverter {
         product.setPrice(request.getPrice());
 
         return product;
+    }
+
+    public static ProductResponse ConvertToProductResponse(Product product) {
+        ProductResponse response = new ProductResponse();
+        response.setId(product.getId());
+        response.setName(product.getName());
+        response.setPrice(product.getPrice());
+
+        return response;
     }
 }
