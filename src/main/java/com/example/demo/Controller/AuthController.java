171867beb550ea044d5ece01ba30fee6ae532a87
YU-SIANG,CHEN <qbb225827455@gmail.com>
2022-09-13 23:06:51 +0800

    API 獲取Token或解析Token

---
+++ src/main/java/com/example/demo/Controller/AuthController.java
@@ -0,0 +1,41 @@
+package com.example.demo.Controller;
+
+import com.example.demo.Auth.AuthRequest;
+import com.example.demo.Auth.JWTService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/auth", produces = MediaType.APPLICATION_JSON_VALUE)
+public class AuthController {
+
+    @Autowired
+    private JWTService jwtService;
+
+    @PostMapping
+    public ResponseEntity<Map<String, String>> issueToken(@Valid @RequestBody AuthRequest request) {
+
+        String token = jwtService.genToken(request);
+        Map<String, String> response = Collections.singletonMap("token", token);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/parse")
+    public ResponseEntity<Map<String, Object>> parseToken(@RequestBody Map<String, String> request) {
+
+        String token = request.get("token");
+        Map<String, Object> reponse = jwtService.parseToken(token);
+
+        return ResponseEntity.ok(reponse);
+    }
+}
