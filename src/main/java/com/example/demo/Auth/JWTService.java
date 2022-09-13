b5bbed74297fa1ae5f79f3b94f6d81b9721c098a
YU-SIANG,CHEN <qbb225827455@gmail.com>
2022-09-13 22:09:57 +0800

    實作 JWT的產生與解析

---
+++ src/main/java/com/example/demo/Auth/JWTService.java
@@ -0,0 +1,75 @@
+package com.example.demo.Auth;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Calendar;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+public class JWTService {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    private final String KEY = "JavaProgrammingBeginner";
+
+    public String genToken(AuthRequest request) {
+
+        // 使用帳號密碼驗證
+        Authentication authentication =
+                new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword());
+
+        // authenticationManager接收到Token後
+        // 底層會透過SecurityConfig中配置的UserDetailsService和passwordEncoder來驗證
+        authentication = authenticationManager.authenticate(authentication);
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MINUTE, 2);
+
+        // 宣告Claims物件放置JWT的內容，例如使用者帳號、到期時間、核發者
+        Claims claims = Jwts.claims();
+        claims.put("username", userDetails.getUsername());
+        claims.setExpiration(calendar.getTime());
+        claims.setIssuer("Test");
+
+        // 產生密鑰
+        Key secretKey = Keys.hmacShaKeyFor(KEY.getBytes());
+
+        // 透過Jwts.builder()得到物件，將內容放入，並用密鑰簽名，呼叫compact()方法，產生Token
+        return Jwts.builder()
+                .setClaims(claims)
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public Map<String, Object> parseToken(String token) {
+
+        // 產生密鑰
+        Key secretKey = Keys.hmacShaKeyFor(KEY.getBytes());
+
+        JwtParser parser = Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build();
+
+        Claims claims = parser
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+}
+
--- src/main/java/com/example/demo/Config/SecurityConfig.java
+++ src/main/java/com/example/demo/Config/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.example.demo.Config;
 
 import com.example.demo.Model.User.UserAuthority;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -40,5 +42,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         auth.userDetailsService(userDetailsService)
                 .passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
