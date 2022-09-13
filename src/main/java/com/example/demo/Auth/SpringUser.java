2b8a785b9487d98650f54a292c7f4d2b3acd0f4d
YU-SIANG,CHEN <qbb225827455@gmail.com>
2022-09-13 19:52:25 +0800

    實作 UserDetails介面

---
+++ src/main/java/com/example/demo/Auth/SpringUser.java
@@ -0,0 +1,56 @@
+package com.example.demo.Auth;
+
+import com.example.demo.Model.User.AppUser;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class SpringUser implements UserDetails {
+
+    private AppUser appUser;
+
+    public SpringUser(AppUser appUser) {
+        this.appUser = appUser;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return appUser.getAuthorities().stream()
+                .map(auth -> new SimpleGrantedAuthority(auth.name()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return appUser.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return appUser.getEmailAddress();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
