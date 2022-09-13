c374511fd30a4a674bc6546cb4ecc6f88f865c22
YU-SIANG,CHEN <qbb225827455@gmail.com>
2022-09-13 22:07:37 +0800

    接收帳號密碼的請求主體

---
+++ src/main/java/com/example/demo/Auth/AuthRequest.java
@@ -0,0 +1,14 @@
+package com.example.demo.Auth;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class AuthRequest {
+
+    @NotBlank
+    private String username;
+    @NotBlank
+    private String password;
+}
